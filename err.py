ab30819c5d785d9a48f23eb73f4087850f4dc3af
Simon Dirmeier <simon.dirmeier@web.de>
2019-06-15 23:15:04 +0200

    Some adds to custom clustering

--- err.py
+++ err.py
@@ -1,52 +1,43 @@
-from matplotlib import pyplot as plt
 import numpy as np
 import pymc3 as pm
 import scipy as sp
-import seaborn as sns
-from pymc3.distributions.dist_math import bound
-from theano import tensor as tt
-import pandas as pd
+import scipy.stats as st
+import theano.tensor as tt
+import seaborn
 
+seaborn.set_style("white")
 
-class Error(pm.Discrete):
-    def __init__(self, theta, alpha, beta, *args, **kwargs):
-        super(Error, self).__init__(*args, **kwargs)
-        self.theta = theta
-        self.alpha = alpha
-        self.beta = beta
-
-    def random(self, point=None, size=None):
-        raise NotImplementedError()
-
-    def logp(self, value):
-        theta = self.theta
-        alpha = self.alpha
-        beta = self.beta
-        return bound(
-          tt.sum(tt.log(value)),
-          value >= 0, value <= 1,
-          alpha >= 0, alpha <= 1,
-          beta >= 0, beta <= 1,
-          theta >= 0, theta <= 1)
-
-
-def stick_breaking(beta):
-    p = tt.concatenate([[1], tt.extra_ops.cumprod(1 - beta)[:-1]])
-    return beta * p
-
-
-K = 30
-data = sp.stats.bernoulli.rvs(.5, size=1000).reshape((100, 10))
 
 with pm.Model() as model:
-    alpha = pm.Gamma('alpha', 1., 1.)
-    beta = pm.Beta('beta', 1., alpha, shape=K)
-    w = pm.Deterministic('w', stick_breaking(beta))
+    mu = pm.Uniform('alpha', 1., 100)
+    like = pm.Poisson('x', mu=mu, observed=st.poisson.rvs(mu=5, size=1000))
+    pm.sample()
 
-    comps = [
-        Error.dist(i, i, i)
-        for i in range(K)
-    ]
-    like = pm.Mixture('like', w=w, comp_dists=comps, observed=data)
-
-    pm.sample(100)
+#
+# def stick_breaking(beta):
+#     p = tt.concatenate([[1], tt.extra_ops.cumprod(1 - beta)[:-1]])
+#     return beta * p
+#
+#
+# K = 5
+# np.random.seed(23)
+# data = sp.stats.bernoulli.rvs(.5, size=250).reshape((50, 5))
+#
+# with pm.Model() as model:
+#     alpha = pm.Gamma('alpha', 1., 1.)
+#     beta = pm.Beta('beta', 1., alpha, shape=K)
+#     w = pm.Deterministic('w', stick_breaking(beta))
+#
+#     error_alpha = pm.Uniform("ea", lower=0, upper=1, shape=K)
+#     error_beta = pm.Uniform("eb", lower=0, upper=1, shape=K)
+#     error_theta = pm.Uniform("et", lower=0, upper=1, shape=K)
+#
+#     comp = pm.DensityDist(
+#       "like", logp=logp(error_theta, error_alpha, error_beta), shape=K)
+#     comp_dists = comp.distribution
+#     comp_dists.mean = comp_dists.mode = np.array([1, 1, 1, 1, 1])
+#
+#     like = pm.Mixture('x', w=w, comp_dists=comp_dists, observed=data)
+#
+# with model:
+#     trace = pm.f(300)
